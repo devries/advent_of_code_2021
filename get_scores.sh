63b3e57ef9e1662e0beee3f45f77c4c297631911
Christopher De Vries <devries@idolstarastronomer.com>
2021-11-12 10:21:23 -0500

    Use AOC_SESSION environment variable to get scores

--- get_scores.sh
+++ get_scores.sh
@@ -1,3 +1,34 @@
 #!/bin/sh
 
-http --session=./session.json https://adventofcode.com/2020/leaderboard/private/view/534400.json > 534400.json
+if [ -z "${AOC_SESSION}" ]; then
+  echo "Must set AOC_SESSION environment variable to the session string."
+  exit 1
+fi
+
+cat > session.json <<EOF
+{
+    "__meta__": {
+        "about": "HTTPie session file",
+        "help": "https://httpie.org/doc#sessions",
+        "httpie": "2.3.0"
+    },
+    "auth": {
+        "password": null,
+        "type": null,
+        "username": null
+    },
+    "cookies": {
+        "session": {
+            "expires": null,
+            "path": "/",
+            "secure": false,
+            "value": "${AOC_SESSION}"
+        }
+    },
+    "headers": {}
+}
+EOF
+
+http --session=./session.json https://adventofcode.com/2021/leaderboard/private/view/534400.json > 534400.json
+
+rm session.json
