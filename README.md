385eedfa78e944b31366c82ed013cade982df7a0
Christopher De Vries <devries@idolstarastronomer.com>
2021-12-04 08:08:07 -0500

    Completed day 4

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Advent of Code 2021
 
 [![Tests](https://github.com/devries/advent_of_code_2021/actions/workflows/main.yml/badge.svg)](https://github.com/devries/advent_of_code_2021/actions/workflows/main.yml)
-![Stars: 6](https://img.shields.io/badge/⭐_Stars-6-yellow)
+![Stars: 8](https://img.shields.io/badge/⭐_Stars-8-yellow)
 
 ## Index
 
 - [Day 1: Sonar Sweep](https://adventofcode.com/2021/day/1) - [part 1](day01_p1), [part 2](day01_p2)
 - [Day 2: Dive!](https://adventofcode.com/2021/day/2) - [part 1](day02_p1), [part 2](day02_p2)
 - [Day 3: Binary Diagnostic](https://adventofcode.com/2021/day/3) - [part 1](day03_p1), [part 2](day03_p2), [part 2 refactor](day03_p2alt)
+- [Day 4: Giant Squid](https://adventofcode.com/2021/day/4) - [part 1](day04_p1), [part 2](day04_p2)
---
+++ day04_p1/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/devries/advent_of_code_2021/utils"
+)
+
+func main() {
+	f, err := os.Open("../inputs/day04.txt")
+	utils.Check(err, "error opening input.txt")
+	defer f.Close()
+
+	r := solve(f)
+	fmt.Println(r)
+}
+
+// Bingo card
+type Card struct {
+	Dim     int   // length of row or column
+	Numbers []int // Numbers on card
+}
+
+func NewCard(d int) *Card {
+	c := Card{d, make([]int, d*d)}
+
+	return &c
+}
+
+func (c *Card) Parse(lines []string) {
+	for i := 0; i < c.Dim; i++ {
+		sequence := strings.Fields(lines[i])
+		for j, s := range sequence {
+			n, err := strconv.Atoi(s)
+			utils.Check(err, "Error parsing card number")
+			c.Numbers[i*c.Dim+j] = n
+		}
+	}
+}
+
+// Check for Bingo
+func (c *Card) Check(selections map[int]bool) bool {
+	// Check rows
+outerRow:
+	for j := 0; j < c.Dim; j++ {
+		for i := c.Dim * j; i < c.Dim*(j+1); i++ {
+			if selections[c.Numbers[i]] == false {
+				continue outerRow
+			}
+		}
+		return true
+	}
+
+	// Check columns
+outerCol:
+	for j := 0; j < c.Dim; j++ {
+		for i := j; i < c.Dim*c.Dim; i += c.Dim {
+			if selections[c.Numbers[i]] == false {
+				continue outerCol
+			}
+		}
+		return true
+	}
+
+	return false
+}
+
+func (c *Card) Score(selection map[int]bool) int {
+	sum := 0
+	for _, v := range c.Numbers {
+		if selection[v] == false {
+			sum += v
+		}
+	}
+
+	return sum
+}
+
+func solve(r io.Reader) int {
+	lines := utils.ReadLines(r)
+
+	pickLine := strings.Split(lines[0], ",")
+	picks := make([]int, 0)
+
+	for _, s := range pickLine {
+		v, err := strconv.Atoi(s)
+		utils.Check(err, "Error converting integer in pick line")
+		picks = append(picks, v)
+	}
+
+	cards := make([]*Card, 0)
+	for i := 2; i < len(lines); i += 6 {
+		c := NewCard(5)
+		c.Parse(lines[i : i+5])
+		cards = append(cards, c)
+	}
+
+	selected := make(map[int]bool)
+	for _, p := range picks {
+		selected[p] = true
+
+		for _, c := range cards {
+			if c.Check(selected) {
+				return c.Score(selected) * p
+			}
+		}
+	}
+
+	return 0
+}
